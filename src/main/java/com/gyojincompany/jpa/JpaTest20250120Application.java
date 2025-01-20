d108dc739aa7276b591d4d247fddff674972fee2
gyojincompany <luckyred@gmail.com>
2025-01-20 12:13:09 +0900

    jpa 설정 및 member Entity 추가

---
+++ src/main/java/com/gyojincompany/jpa/JpaTest20250120Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTest20250120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTest20250120Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/jpa/JpaTest20250120ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTest20250120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
